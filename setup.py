049b0adbd2bd84db9f258f2a7dc7c1b55c6eb9eb
André Pedersen <andrped94@gmail.com>
2023-04-04 10:44:35 +0200

    bump 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
       name='sapai_gym',
-      version='0.1.0',
+      version='0.1.1',
       packages=find_packages(),
       install_requires=[
           "sapai @ git+https://github.com/manny405/sapai.git@main",

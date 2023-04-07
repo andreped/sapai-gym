507a0ea90aa7c59b382b11e820127634cce97d45
André Pedersen <andrped94@gmail.com>
2023-04-07 17:05:14 +0200

    Moved deps to external requirements.txt

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt', 'r', encoding='utf-16', errors='ignore') as ff:
+    required = ff.read().splitlines()
+
 setup(
       name='sapai_gym',
       version='0.1.1',
       packages=find_packages(),
-      install_requires=[
-          "sapai @ git+https://github.com/andreped/sapai.git@update-stats",
-          "https://github.com/andreped/gym/releases/download/v0.21.0-binary/gym-0.21.0-py3-none-any.whl",
-          "scikit-learn"
-      ]
+      install_requires=required,
 )
